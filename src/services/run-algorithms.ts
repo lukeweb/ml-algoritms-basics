7d6ccf5227f336770860c48af9ec114b413e8302
Łukasz Pstrowski <l.pstrowski@clearcode.cc>
2022-02-21 17:40:07 +0100

    KNN algorithm implementation with randomly generated test data

--- src/services/run-algorithms.ts
+++ src/services/run-algorithms.ts
@@ -1,9 +1,11 @@
 import { isArray } from 'lodash';
 import { Logger } from 'pino';
+import { KNNAlgorithm } from '../algorithms/knn';
 import { KMeansAlgorithm } from '../algorithms/k-means';
 
 export const runAlgorithms = (logger: Logger): void => {
   runKMeansAlgorithm(logger.child({ name: 'K-means algorithm' }));
+  runKNNAlgorithm(logger.child({ name: 'K-NN algorithm' }));
 };
 
 export const runKMeansAlgorithm = (logger: Logger): void => {
@@ -26,14 +28,52 @@ export const runKMeansAlgorithm = (logger: Logger): void => {
 
   logger.info('Process result:');
   Object.keys(result).forEach((key: string) => {
-    if (isArray(result[key])) {
-      logger.info(`${key}:`);
-      result[key].map((record: number, index: number) =>
-        logger.info(`${index + 1}:[${record}]`),
-      );
-    } else {
-      logger.info(`${key}: ${result[key]}`);
-    }
+   logger.info({ [key]: result[key] });
   });
   logger.info('=====================  END K-MEANS  =======================');
+};
+
+export const runKNNAlgorithm = (logger: Logger) => {
+  const labels: string[] = ['Male', 'Female'];
+  const testData: { data: number[][]; labels: string[] } = {
+    data: [],
+    labels: [],
+  };
+
+  for (let i = 0; i < 1000; i++) {
+    testData.data.push([
+      Math.floor(Math.random() * (220 - 150 + 1) + 150),
+      Math.floor(Math.random() * (120 - 55 + 1) + 55),
+    ]);
+
+    testData.labels.push(labels[0]);
+  }
+
+  for (let i = 0; i < 1000; i++) {
+    testData.data.push([
+      Math.floor(Math.random() * (185 - 140 + 1) + 140),
+      Math.floor(Math.random() * (100 - 45 + 1) + 45),
+    ]);
+
+    testData.labels.push(labels[1]);
+  }
+
+  logger.info('===================  START KNN  =======================');
+
+  const knn = new KNNAlgorithm(3, testData.data, testData.labels);
+
+  logger.info('Process result:');
+  const firstResult = knn.predict([181, 65]);
+  const secondResult = knn.predict([161, 58]);
+  logger.info('-------------  FIRST RESULT -------------------------- ');
+  Object.keys(firstResult).forEach((key) => {
+    logger.info({ [key]: firstResult[key] });
+  });
+
+  logger.info('-------------  SECOND RESULT -------------------------- ');
+  Object.keys(secondResult).forEach((key) => {
+    logger.info({ [key]: firstResult[key] });
+  });
+
+  logger.info('===================  END KNN  =======================');
 };
